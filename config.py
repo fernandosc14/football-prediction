22d4899b0284760063cd2a392a1a36535bd59116
Fernando <fscasas05@gmail.com>
2025-09-06 04:56:35 +0100

    fix: add default Redis URL for local and test environments

--- config.py
+++ config.py
@@ -5,4 +5,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 LAST_UPDATE_KEY = "last_update_timestamp"
-redis_client = redis.from_url(os.getenv("REDIS_URL"))
+redis_url = os.getenv("REDIS_URL", "redis://localhost:6379/0")
+redis_client = redis.from_url(redis_url)
