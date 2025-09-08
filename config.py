e3985916473c4b2908252867778a60684d48d609
Fernando <fscasas05@gmail.com>
2025-09-08 01:16:54 +0100

    test: remove API endpoint tests with authentication issues

--- config.py
+++ config.py
@@ -6,4 +6,6 @@ load_dotenv()
 
 LAST_UPDATE_KEY = "last_update_timestamp"
 redis_url = os.getenv("REDIS_URL")
+if not redis_url:
+    raise RuntimeError("REDIS_URL environment variable is not set")
 redis_client = redis.from_url(redis_url)
