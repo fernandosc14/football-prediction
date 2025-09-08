148df35d97932c8905cbdbcdd8ecf201de217f1c
Fernando <fscasas05@gmail.com>
2025-09-08 01:04:58 +0100

    config: remove default localhost fallback for REDIS_URL

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 LAST_UPDATE_KEY = "last_update_timestamp"
-redis_url = os.getenv("REDIS_URL", "redis://localhost:6379/0")
+redis_url = os.getenv("REDIS_URL")
 redis_client = redis.from_url(redis_url)
